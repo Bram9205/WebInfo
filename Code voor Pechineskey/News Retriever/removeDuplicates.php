c1e6adcb5ef6ea3b4100d5a8922b030f0251933a
Jasper Selman <japserselman@gmail.com>
2015-10-25 20:38:17 +0100

    mappen guus samengevoegd

--- Code voor Pechineskey/News Retriever/removeDuplicates.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'src/Database.php';
-
-echo "Remove all duplicate title-siteroot rows from database\n";
-
-$removed = Database::removeDupicates();
-
-echo "Finished. Removed $removed rows\n";
